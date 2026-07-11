05f9cb5f916bc0eb6181b9d051c481d195e9d8fa
Ardiansyah Martin <autologika.id@gmail.com>
2026-07-11 16:15:55 +0700

    adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({});
+export default defineConfig({
+
+  site: "https://autologika.id",
+
+  integrations: [
+    sitemap()
+  ]
+
+});
